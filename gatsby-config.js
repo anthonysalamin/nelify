dc85ebc4b5ada12396d0342e0577d21423a948d2
Anthony Salamin <37147870+anthonysalamin@users.noreply.github.com>
2021-07-08 20:32:35 +0200

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "gatsby with strapi deployed on netlify",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        // apiURL: process.env.API_URL ? "https://gatsby-strapi-heroku.herokuapp.com" : "http://localhost:1337", // production environement
+        apiURL: `http://localhost:1337`, // default developement environement
+        queryLimit: 1000,
+        collectionTypes: [`article`, `user`], // modify 🍋
+        singleTypes: [], // modify 🍓
+      },
+    },
+  ],
 };
